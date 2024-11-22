6951a0fb9456b94b8606951a6e152419133321a0
ugfire <ugfirecompany@gmail.com>
2024-11-22 14:48:58 +0300

    added map img

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MapComponent } from "../map/map.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MapComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
