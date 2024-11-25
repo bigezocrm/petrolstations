0becd10556030f617f821d6a9adbbb35a7b3181e
ugfire <ugfirecompany@gmail.com>
2024-11-25 15:27:59 +0300

    added logout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MapComponent } from "../map/map.component";
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { SupabaseService } from '../superbase.service';
 
 @Component({
     selector: 'app-dashboard',
@@ -11,7 +13,21 @@ import { CommonModule } from '@angular/common';
 export class DashboardComponent {
     showMap = false;  // Track map visibility
 
+    constructor(private router: Router, private supabaseService: SupabaseService) {}
+
+  logout() {
+    this.supabaseService.logout()
+      .then(() => {
+        // Redirect the user to the login page after logging out
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => {
+        console.error('Logout failed:', error);
+      });
+  }
+
     toggleMapVisibility() {
       this.showMap = !this.showMap;  // Toggle the visibility
     }
+
 }
